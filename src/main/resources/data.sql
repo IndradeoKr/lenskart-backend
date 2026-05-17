fab56fade79d0e41f3e5dd606e7a2a030c567cdd
IndradeoKr <indradeokumar614idk@gmail.com>
2026-05-17 14:41:57 +0530

    Drop legacy unique constraint on cart_id and defer jpa initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- Drop the old unique constraint on cart_id that was created when the relationship was @OneToOne.
+-- Hibernate ddl-auto=update does not remove old constraints, so we must do it manually.
+ALTER TABLE orders DROP CONSTRAINT IF EXISTS uks1sr8a1rkx80gwq9pl0952dar;
